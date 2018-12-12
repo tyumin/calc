a081b6be3d599fd58317c53d4f6f5f99f64d32f3
Yury Tyumin <tyumin@gmail.com>
2018-12-12 22:30:57 +0200

    prime

--- 1.js
+++ 1.js
@@ -1,7 +1,10 @@
 'use strict';
 
-var a = 2;
-
-var x = 1 + (a *= 2);
-
-alert(x);
+for(var i=2; i<=100; i++){
+  var prime = true;
+  for(var j=2; j<i; j++){
+    if(!(i % j)) prime = false
+    }
+    // alert("Сравним "+i+' '+j+'='+x);
+  if(prime) alert(i);
+}
