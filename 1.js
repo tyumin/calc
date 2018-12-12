dad5c881c22adb8adfb8c14639c4a12dfe7d3e86
Yury Tyumin <tyumin@gmail.com>
2018-12-12 22:51:41 +0200

    13

--- 1.js
+++ 1.js
@@ -1,10 +1,12 @@
 'use strict';
 
-for(var i=2; i<=100; i++){
-  var prime = true;
+main: for(var i=2; i<=100; i++){
+  // var prime = true;
   for(var j=2; j<i; j++){
-    if(!(i % j)) prime = false
+    if(!(i % j)) continue main;
     }
     // alert("Сравним "+i+' '+j+'='+x);
-  if(prime) alert(i);
+  // if(prime) alert(i);
+
+  alert(i);
 }
