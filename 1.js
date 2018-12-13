69ba23d1e7ac2b12d949e11eeab3221f626e5b6c
Тюмин Юрий Александрович <tyua@pro-pharma.com.ua>
2018-12-13 10:17:11 +0200

    a

--- 1.js
+++ 1.js
@@ -1,12 +1,8 @@
 'use strict';
 
 main: for(var i=2; i<=100; i++){
-  // var prime = true;
   for(var j=2; j<i; j++){
     if(!(i % j)) continue main;
     }
-    // alert("Сравним "+i+' '+j+'='+x);
-  // if(prime) alert(i);
-
   alert(i);
 }
