a15507f14b1a27e034a3bbac936298305fddee77
Yury Tyumin <tyumin@gmail.com>
2018-12-15 19:38:25 +0200

    qw

--- 1.js
+++ 1.js
@@ -1,28 +1,28 @@
 'use strict';
 
 function g(a,b){
-    return a+b;
+  return a+b;
 }
 
 alert(g);
 
 function fib1(n) {
-    //alert(n);
-    if(n <= 2) return 1;
-    return;
+  //alert(n);
+  if(n <= 2) return 1;
+  return;
 }
 
 function fib(n){
-    var prev = 0;
-    var result = 1;
-    for (var i = 1; i < n; i++) {
+  var prev = 0;
+  var result = 1;
+  for (var i = 1; i < n; i++) {
     // alert('prev - ' + prev + ' result - ' + result) ;
-        var tmp = result;
-        result = result + prev;
-        prev = tmp;
+    var tmp = result;
+    result = result + prev;
+    prev = tmp;
     // alert('result - ' + result);
-    }
-    return result;
+  }
+  return result;
 }
 
 alert( fib(77) ); // (*)
