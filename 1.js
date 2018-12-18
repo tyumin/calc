a305e3a11cf347ee5d608edd796e531499bf5805
Тюмин Юрий Александрович <tyua@pro-pharma.com.ua>
2018-12-18 10:57:38 +0200

    test

--- 1.js
+++ 1.js
@@ -1,3 +1,8 @@
 'use strict';
 
-alert('Hello World!!'.length);
+function ucFirst(str){
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+//var str = prompt('str?','');
+// alert(ucFirst(str));
--- test.js
+++ test.js
@@ -1,7 +1,10 @@
-describe('pow', function() {
 
-  it('возводит в n-ю степень', function() {
-    assert.equal(pow(2, 3), 8);
+
+describe('ucFirst', function() {
+  it('Первая буква заглавная в слове Привет ', function(){
+    assert.equal(ucFirst('привет'), 'Привет');
   });
-
+  it('Проверка на пустую строку ', function(){
+    assert.equal(ucFirst(''), '');
+  });
 });
